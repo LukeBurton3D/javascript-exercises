d59afef8ced4b36d97f54b34eea4c918410e79c1
LukeBurton3D <lukeburton3d@outlook.com>
2025-07-22 13:15:18 +0100

    JS challenges 6 - 11

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const divByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const fourHun = year % 400 === 0;
+    
+    if (divByFour && (!isCentury || fourHun)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
