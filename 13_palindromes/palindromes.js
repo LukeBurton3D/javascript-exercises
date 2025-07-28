ac06d833931266a4b8197514000f3284a0ae233e
LukeBurton3D <lukeburton3d@outlook.com>
2025-07-28 14:44:08 +0100

    javascript exercises palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumeric = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    const cleanedString = string.toLowerCase()
+                                .split('')
+                                .filter((character) => alphanumeric.includes(character))
+                                .join('');
+    const reversedString = cleanedString.split('').reverse().join('');
 
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
